2545f59f97f5ba2468d33f46637d5fd9079e80a2
Karina <edshdgd>
2015-04-10 13:19:58 +0400

    c Expresessedskss

--- server.js
+++ server.js
@@ -1,11 +1,15 @@
-var http = require("http"),
-    server;
-
+var express = require("express"),
+	http = require("http"),
+	app = express();
+    server,
+    port=process.env.PORT || 1337;
+    
+app.use(express.static(__dirname + "/client"));
 server = http.createServer(function (req, res) {
     res.writeHead(200, {"Content-Type": "text/plain"});
     res.end("Hello World!\n");
 });
 
-server.listen(1337);
+server.listen(port);
 
 console.log("Server running on port 3000");
