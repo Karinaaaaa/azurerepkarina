cfdc6c5d82bff5016adacea594845be34e7d482a
Karina <edshdgd>
2015-04-10 12:37:33 +0400

    c Expresessets

--- app/server.js
+++ app/server.js
@@ -1,5 +1,6 @@
 var http = require("http"),
     server;
+    var port = process.env.PORT||1337;
 
 server = http.createServer(function (req, res) {
     res.writeHead(200, {"Content-Type": "text/plain"});
