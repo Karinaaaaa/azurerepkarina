5f18bd321d5aa4fcaf45a5575312a289d2928ffc
Karina <edshdgd>
2015-04-29 10:56:20 +0400

    dsegesesggse

--- server.js
+++ server.js
@@ -50,7 +50,7 @@ app.post("/todos", function (req, res) {
   var newToDo = req.body;
   //console.log(newToDo);
   res.json({"message":"Вы разместили комментарий на сервере!" + newToDo.Id});
-  toDos.push(newToDo.Id);
+  //toDos.push(newToDo.Id);
   // отправляем простой объект//
-  res.json({"message":"Вы разместили комментарий на сервере в массиве toDos" + toDos[0]});
+  //res.json({"message":"Вы разместили комментарий на сервере в массиве toDos" + toDos[0]});
 });
