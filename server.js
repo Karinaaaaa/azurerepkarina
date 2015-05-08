85d04e1f58e6ddd261c53d4b6a86d2280f448eea
Karina <edshdgd>
2015-05-08 13:37:01 +0400

    edsgdg

--- server.js
+++ server.js
@@ -1,36 +1,12 @@
 var express = require("express"),
 app = express(),
+stream = require("./stream.js"),
 http = require("http"),
 port = process.env.PORT || 1337;
-var ntwitter = require("ntwitter"),
-
-credentials = require("./credentials.json"),
-
-twitter = ntwitter(credentials);
 
 
-var coolObject = {"my": 12345};
+var coolObject = {my: 12345};
 var toDos = [];
-// настроим поток twitter с тремя параметрами, разделенными запятыми
-
-twitter.stream(
-// первый параметр — строка
-"statuses/filter",
-
-// второй параметр — объект, содержащий массив со словами, к. мы ищем
-{ "track": ["awesome", "cool", "rad", "gnarly", "groovy" ]},
-
-// третий параметр — обратный вызов, срабатывающий, когда поток создан
-function(stream) {
-
-	stream.on("data", function(tweet) {
-	console.log(tweet.text);
-
-	});
-}
-);
-
-
 
 //app.use(express.urlencoded());
 app.use(express.static(__dirname + "/client"));
@@ -38,8 +14,8 @@ app.use(express.static(__dirname + "/client"));
 // создадим HTTP-сервер на базе Express
 http.createServer(app).listen(port);
 
- app.get("/someway.json", function (req, res) {
-res.json(coolObject);
+app.get("/someway.json", function (req, res) {
+res.json(stream);
 });
 
 
@@ -47,9 +23,9 @@ app.use(express.bodyParser());
 app.post("/todos", function (req, res) {
   // сейчас объект сохраняется в req.body
   var newToDo = req.body;
-  //console.log(newToDo);
-  res.json({"message":"Вы разместили комментарий на сервере!" + newToDo.Id});
-  //toDos.push(newToDo.Id);
-  // отправляем простой объект//
-  //res.json({"message":"Вы разместили комментарий на сервере в массиве toDos" + toDos[0]});
+  console.log(newToDo);
+  toDos.push(newToDo);
+  // отправляем простой объект
+  res.json({"message":"Вы разместили данные на сервере!"});
 });
+
