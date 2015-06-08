490025d5a958eeb4a8efc8798cc1c905b9075b01
Karina <edshdgd>
2015-06-08 11:35:06 +0400

    efefef

--- client/javascripts/new.js
+++ client/javascripts/new.js
@@ -5,64 +5,90 @@ return num*num;
 });
 
 
-var main = function () {
+var main = function () 
+{
 "use strict";
 
 var $photo;
 var $IMGcom;
 var url = "http://api.flickr.com/services/feeds/photos_public.gne?" +
 "tags=dogs&format=json&jsoncallback=?";
+var someAppOne11object;
 
 var someObject;
 
-// setInterval(function () {
-//     $.getJSON("/someway.json", function (fromServer) {
-//     // Сейчас "fromServer" становится объектом, возвращаемым маршрутом someway.json
-//     console.log(fromServer);
-// 	});
-//   }, 5000);
+setInterval(function () 
+{
+    $.getJSON("/someway.json", function (fromServer) 
+    {
+    // Сейчас "fromServer" становится объектом, возвращаемым маршрутом someway.json
+    console.log(fromServer);
+	});
+  }, 5000);
 
-var Clake = function() {
-	//console.log("Передаём на сервер");
-	consoie.log(typeof(someObject));
-	console.log(someObject);
-	$.post("todos", someObject, function (data) {
+var Clake = function() 
+{
+	    console.log(someObject);
+	        console.log(typeof(someObject));
+
+	$.post("todos", someObject, function (data) 
+	{
     // это обратный вызов, выполняется при ответе сервера
-    //console.log("Получение данных с сервера");
-    window.alert(data);
-    //window.alert("Получение "+ response);
+
+    console.log("Получение данных с сервера");
+    console.log(data);
+
     });
 };
 
 
 
 
+var sun = function () 
+{
+//
+var tweet = $.getJSON("/someway.json", function sun(fromServer) 
+	{
 
-$.getJSON(url, function (flickrResponse) {
-flickrResponse.items.forEach(function (photo) {
+someAppOne11object = fromServer;
+	});
+
+//}, 5000);
+return fromServer;
+
+};
+
+
+
+
+$.getJSON(url, function (flickrResponse) 
+{
+flickrResponse.items.forEach(function (photo) 
+	{
     var $img = $("<img>").hide();
 
     	$img.on("click", function ()
 		{
 		var that = this;
 
-	$("img").hide(1000,  function() {
+	$("img").hide(1000,  function() 
+			{
 			$img.show();
 			$IMGcom = $img.attr("src");
 
 
-});
+			});
 		
 		
 		
-    });
+    	});
 $img.attr("src", photo.media.m);
 $("main").append($img);
 $img.fadeIn();
 
 
 
-});
+	});
 
 });
 
@@ -99,6 +125,9 @@ $(".tabs a:nth-child(1)").on("click", function ()
     
 		if(tabnumber===1)
 		{
+var cloud = sun();
+$(" main .content").prepend($("<p>").text(cloud));
+
 		
 		}
 			else if (tabnumber===2)
@@ -114,36 +143,39 @@ $(".tabs a:nth-child(1)").on("click", function ()
    };
 
 
-   $(".comment-input input").on("keypress", function (event) {
+   $(".comment-input input").on("keypress", function (event) 
+{
 if (event.keyCode === 13) 
 	{
 addCommentFromInputBox();
+Clake();
 	}
 });
 
- $(".comment-input button").on("click", function (event) {
+ $(".comment-input button").on("click", function (event) 
+ {
     addCommentFromInputBox();
     Clake();
   });
 
 
    var addCommentFromInputBox = function () 
-   {
+{
    	 	
- 	if ($(".comment-input input").val() !== "") {
+ 	if ($(".comment-input input").val() !== "") 
+ 	{
  		var $new_comment = $("<p>");
 	    $new_comment.text($(".comment-input input").val());
 	    $(".comments").append($new_comment);
 	    $new_comment.fadeIn(1500);
 	    var $comms = $(".comment-input input").val();
 	    $(".comment-input input").val("");
-	    var jsonString = '{ "Id" : $IMGcom, "Comment" : $comms }';
-		
-		someObject=jsonString;
-
+	    var jsonString = { "Id" : $IMGcom, "comment" : $comms };
+	//var jsonString2 = JSON.stringify(jsonString);
+	//console.log(jsonString2);
+	someObject=jsonString;
 	}
-
-	};
+};
 
 
 if (event.keyCode === 13) 
@@ -154,8 +186,7 @@ if (event.keyCode === 13)
 	    $new_comment.fadeIn(2000);	
 	    $(".comment-input input").val("");
 
-};
-
 	};
+};
 
 $(document).ready(main);
--- server.js
+++ server.js
@@ -4,26 +4,17 @@ app = express(),
 http = require("http"),
 port = process.env.PORT || 1337;
 
-
 var mongoose= require('mongoose');
 var connectionString = process.env.CUSTOMCONNSTR_MONGOLAB_URI;
 mongoose.connect(connectionString);
 
-var CommSchema = mongoose.Schema
-({
 
-var coolObject = {my: 12345};
-var toDos = [];
-
-var mongoose=require("mongoose"),
-//mongoUrl="mongodb://localhost/test";
-var ConnectoinString=process.env.CUSTOMCONNSTR_MONGOLAB_URL;
-//app.use(express.urlencoded());
-app.use(express.static(__dirname + "/client"));
 app.use(express.bodyParser());
 
+var CommSchema = new mongoose.Schema
+({
 
-Id : String,
+id : String,
 
 commentariy : String
 
@@ -31,63 +22,67 @@ commentariy : String
 
 var com1 = mongoose.model("Comm", CommSchema);
 
+// var c1 = new com1({"id":"Doggie", "commentariy":"goodie"});
 
-var cc = function(Doggie,goodie){
-	var c1 = new com1({"title":Doggie, "commentariy":goodie});
-	c1.save(function (err) {
+// c1.save(function (err) 
+// {
 
-if (err !== null) 
-		{
-console.log(err);
-console.log("Объект не был сохранен!");
-		} else 
-	
-console.log("Объект был сохранен!");
-	}
-	)
-};
+// if (err !== null) 
+// 		{
+// console.log(err);
+// console.log("Объект не был сохранен!");
+// 		} else 
+// 	{
+// console.log("Объект был сохранен!");
+// 	}
 
+// });
 
-mongoose.connect (ConnectoinString);
-var CommSchema = mongoos.Schema({
-title : String,
-commentariy : String
-
-});
-
-var ff= function(){
-com1.find({"title" : "bad"}, function (err, comments) {
-
-comments.forEach(function (com) {
-com.commentariy = "the best!";
-
-com.save(function (err) 
+//"id" : "http://farm1.staticflickr.com/543/17860695134_457dc23186_m.jpg"
+ function cf () {
+ 	var comM = "sssss";
+ 	com1.find({"id" : "http://farm1.staticflickr.com/543/17860695134_457dc23186_m.jpg"}, function (err, comments) 
+	{
+		if(err)
 			{
+				comM=err;
+			}
 
-	if (err)	
-				{
+comments.forEach(function (com) 
+		{
+			comM = com[commentariy];
+//com.commentariy = "the best!";
+
+// com.save(function (err) 
+// 			{
+
+// 	if (err)	
+// 				{
 
 
-console.log(err);
+// console.log(err);
 
-				}
+// 				}
+// 				else {
+// 					}
 
-			});
+
+// 			});
 
 		});
 
 	});
+	return comM;
 };
-
-
-/*com1.remove({"title":"Doggie", "commentariy":"goodie"}, function(err)
+//function ca () {return "sdjf";}
+/*com1.remove({"id":"Doggie", "commentariy":"goodie"}, function(err)
 {
 	if(err!== null)
 	{
 		console.log(err);
 	}
-});
-*/
+});*/
+
 var coolObject = {my: 12345};
 var toDos = [];
 
@@ -99,27 +94,31 @@ http.createServer(app).listen(port);
 
 app.get("/someway.json", function (req, res)
 {
-res.json("stream");
+	var jhjhn = cf();
+res.json(jhjhn);
 });
 
 
 
-var fff= function(newId,newcomment){
 
-var comff = new com1({ "Id":newId, "commentariy":newcomment });
-var comff.save(function (err,result) 
+ var ToDB = function (newId, newComment)
+ 		{
+ 			var com11 = new com1  ({"id" : newId, "commentariy" : newComment });
+ 			com11.save(function (err, result) 
 {
-  if (err !== null) {
-    // РµСЃР»Рё РѕР±СЉРµРєС‚ РЅРµ Р±С‹Р» СѓСЃРїРµС€РЅРѕ СѓРґР°Р»РµРЅ    console.log(deletion);
-    console.log(err);
-  }
-  else{
 
-console.log("объект был сохранен");
-}
+if (err !== null) {
+console.log(err);
+		}
+		else 
+	{
+console.log("Объект был сохранен!");
+	}
 }
 
- ); };
+
+);
+ 		};
 
 
 
@@ -128,13 +127,8 @@ app.post("/todos", function (req, res)
   // сейчас объект сохраняется в req.body
   var newToDo = req.body;
   console.log(newToDo);
-  toDos.push(newToDo);
-   fff(newToDo.Id,newToDo.Comment);
+ 
+  ToDB(newToDo.Id, newToDo.comment);
   // отправляем простой объект
   res.json(newToDo);
 });
-
-
-// ЈРґР°Р»РёС‚СЊ
-
-};
